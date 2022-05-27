e44d560366924cae82e115a2092625265ecfb6f5
caov10 <camiloaov10@gmail.com>
2022-05-26 21:50:04 -0500

    update5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,7 @@
-//import React from "react";
+import React from "react";
 
-//function App(){
-  //      return <h1>Hello React!!</h1>
-//}
-//export default App;
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './styles/global.scss';
-
-ReactDOM.render(<App />, document.getElementById('app'));
+function App(){
+        return <h1>Hello React!!</h1>
+}
+export default App;
